81e113f44262ceae2013dfec6dc2ba56b5fcd4e9
vikagladka <vikagladka@ukr.net>
2024-08-30 03:23:06 +0300

    Fix webpack configuration

--- src/index.js
+++ src/index.js
@@ -1,1 +1,1 @@
-console.log('Hello from webpack!');
+import './scss/main.scss';
--- webpack.config.js
+++ webpack.config.js
@@ -1,62 +1,68 @@
 const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin'); // Додайте цей рядок
 
 module.exports = {
-    entry: './src/index.js',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-        assetModuleFilename: 'images/[name][ext][query]',
-        publicPath: '/',
-        clean: true,
-    },
-    module: {
-        rules: [
-            {
-                test: /\.scss$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    'css-loader',
-                    'sass-loader',
-                ],
-            },
-            {
-                test: /\.css$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    'css-loader',
-                ],
-            },
-            {
-                test: /\.(png|jpg|gif|svg)$/,
-                type: 'asset/resource',
-                generator: {
-                    filename: 'images/[name][ext][query]',
-                },
-            },
-            {
-                test: /\.html$/,
-                use: ['html-loader'],
-            },
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader',
         ],
-    },
-    devServer: {
-        static: {
-            directory: path.join(__dirname, 'dist'),
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'images/[name][ext]', // Зберігаємо зображення у папці dist/images
         },
-        compress: true,
-        port: 9000,
-    },
-    plugins: [
-        new CleanWebpackPlugin(),
-        new MiniCssExtractPlugin({
-            filename: './src/css/[name].css',
-        }),
-        new HtmlWebpackPlugin({
-            template: './src/index.html',
-            inject: 'body',
-        }),
+      },
     ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].css',
+    }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: 'src/images', to: 'images' }, // Копіюємо папку зображень
+        { from: 'src/css/reset.css', to: 'css/reset.css' }, // Копіюємо reset.css
+      ],
+    }),
+  ],
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    compress: true,
+    port: 9000,
+    open: true,
+    hot: true,
+    devMiddleware: {
+      writeToDisk: true,
+    },
+  },
+  mode: 'development',
 };
